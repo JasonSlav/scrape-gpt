6687698780c61397e8cd388d63eefd9d9223b7ad
JasonSlav <joyapus257@gmail.com>
2025-03-13 09:05:28 +0700

    .env.example & config.py templates

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+DBMS_NAME = os.getenv("DBMS_NAME", "postgresql")
 DB_NAME = os.getenv("DB_NAME", "dbname")
 DB_USER = os.getenv("DB_USER", "username")
 DB_PASS = os.getenv("DB_PASS", "password")
@@ -11,5 +12,5 @@ DB_PORT = os.getenv("DB_PORT", "5432")
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    SQLALCHEMY_DATABASE_URI = f"{DBMS_NAME}://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
