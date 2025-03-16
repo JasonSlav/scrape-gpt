3abb0c8d6f7729f6cfb979f58e07d6cf0a53f4b2
JasonSlav <joyapus257@gmail.com>
2025-03-16 17:39:00 +0700

    added description and preprocess

--- models/conversation.py
+++ models/conversation.py
@@ -5,8 +5,10 @@ from models import db
 class Conversation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    message = db.Column(db.Text, nullable=False)
+    link = db.Column(db.Text, nullable=False)
     response = db.Column(db.Text, nullable=False)
+    preprocessed = db.Column(db.Text, nullable=False)
+    description = db.Column(db.Text, nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
     updated_at = db.Column(db.DateTime, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
 
@@ -14,16 +16,18 @@ class Conversation(db.Model):
         return {
             "id": self.id,
             "user_id": self.user_id,
-            "message": self.message,
+            "link": self.link,
             "response": self.response,
+            "preprocessed": self.preprocessed,
+            "description": self.description,
             "created_at": self.created_at.isoformat() if self.created_at else None,
             "updated_at": self.updated_at.isoformat() if self.updated_at else None
         }
 
 @event.listens_for(Conversation, "before_insert")
 def before_insert(mapper, connection, target):
-    print(f"New Conversation Created: {target.message}")
+    print(f"New Conversation Created: {target.link}")
 
 @event.listens_for(Conversation, "before_update")
 def before_update(mapper, connection, target):
-    print(f"Conversation Updated: {target.message}")
+    print(f"Conversation Updated: {target.link}")
