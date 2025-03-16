a920f254c5e54889a29a6837efba71ab851c1f35
JasonSlav <joyapus257@gmail.com>
2025-03-16 17:40:05 +0700

    integrated login with frontend

--- routes/auth.py
+++ routes/auth.py
@@ -1,5 +1,5 @@
-from flask import request, jsonify, Blueprint
-from flask_login import login_user, login_required, logout_user
+from flask import request, jsonify, Blueprint, url_for, redirect, flash
+from flask_login import login_user, login_required, logout_user, current_user
 from models import db
 from models.user import User
 
@@ -7,33 +7,44 @@ auth_bp = Blueprint("auth", __name__)
 
 @auth_bp.route("/register", methods=["POST"])
 def register():
-    data = request.get_json()
-    if not data.get("username") or not data.get("password"):
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if not username or not password:
         return jsonify({"error": "Username dan password harus diisi"}), 400
 
-    if User.query.filter_by(username=data["username"]).first():
+    if User.query.filter_by(username=username).first():
         return jsonify({"error": "Username sudah terdaftar"}), 400
 
-    new_user = User(username=data["username"])
-    new_user.set_password(data["password"])
+    new_user = User(username=username)
+    new_user.set_password(password)
     db.session.add(new_user)
     db.session.commit()
 
-    return jsonify({"message": "User berhasil didaftarkan"}), 201
+    login_user(new_user, remember=True)
+    # flash("Register berhasil!", "success")
+    return redirect(url_for("home"))
 
 @auth_bp.route("/login", methods=["POST"])
 def login():
-    data = request.json
-    user = User.query.filter_by(username=data.get("username")).first()
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if not username or not password:
+        return jsonify({"error": "Username dan password harus diisi"}), 400
+
+    user = User.query.filter_by(username=username).first()
     
-    if user and user.check_password(data.get("password")):
+    if user and user.check_password(password):
         login_user(user, remember=True)
-        return jsonify({"message": "Login Sukses!"}), 200
+        # flash("Login berhasil!", "success")
+        return redirect(url_for("home"))
     
-    return jsonify({"error": "Username atau password salah"}), 401
+    flash("Username atau password salah!", "error")
+    return redirect(url_for("index"))
 
 @auth_bp.route("/logout", methods=["POST"])
 @login_required
 def logout():
     logout_user()
-    return jsonify({"message": "Logout berhasil!"}), 200
+    return redirect(url_for("index"))
